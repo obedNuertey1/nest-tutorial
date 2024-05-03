1fa070a1811dd0a8c15d26cb22f36074269b664d
Obed Nuertey <onuertey1997@gmail.com>
2024-05-03 16:15:21 +0000

    feat: added stufs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsModule } from './cats/cats/cats.module';
+import { CatsModule } from './cats/cats.module';
 
 @Module({
   imports: [CatsModule],
