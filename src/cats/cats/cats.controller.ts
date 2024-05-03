aa621474756f0064712707ddd921f3242db4b731
Obed Nuertey <onuertey1997@gmail.com>
2024-05-03 14:45:07 +0000

    feat: added codebase for request payloads

--- src/cats/cats/cats.controller.ts
+++ src/cats/cats/cats.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateCatDto } from './create-cat.dto';
 
 @Controller('cats')
 export class CatsController {
     @Post()
-    create(): string{
+    async create(@Body() createCatDto:CreateCatDto): Promise<string>{
         return 'This action adds a new cat';
     }
 
